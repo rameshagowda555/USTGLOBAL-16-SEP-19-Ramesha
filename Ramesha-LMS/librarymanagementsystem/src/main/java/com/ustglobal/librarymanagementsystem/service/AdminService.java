f7cfb7c3297cf2bf988737dfc3aa461ed513f1a4
ramehagowda555 <rameshamahesha3@gmail.com>
2019-11-18 06:58:21 +0530

    LibraryManagementSystem Backend Complete Code By-Ramesha

---
+++ Ramesha-LMS/librarymanagementsystem/src/main/java/com/ustglobal/librarymanagementsystem/service/AdminService.java
@@ -0,0 +1,22 @@
+package com.ustglobal.librarymanagementsystem.service;
+
+import java.util.List;
+
+import com.ustglobal.librarymanagementsystem.dto.UserBean;
+
+public interface AdminService {
+	
+	public boolean registerUser(UserBean bean);
+	
+	public UserBean userLogin(int userId,String userPassword);
+	public boolean addStudent(UserBean bean);
+	public boolean removeStudent(int userId);
+	public UserBean searchUser(int userId);
+	public boolean updateStudent(UserBean bean);
+	public List<UserBean> searchAllUser();
+	
+	public boolean addLibrarian(UserBean bean);
+	public boolean removeLibrarian(int userId);
+	public boolean updateLibrarian(UserBean bean);
+	
+}
